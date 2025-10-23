4a6038f8056970bda24e7e65984d33c6794e0831
Richard Lehane <richard@itforarchivists.com>
2025-10-23 21:33:15 +1100

    updates

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -3,8 +3,9 @@ import 'dart:convert';
 
 List<String> AsContext(Uint8List list) {
   final int num = list.first;
+  final int _ = list.elementAt(1); // num of terms/classes
   List<String> ret = List.filled(num, "");
-  int index = 1;
+  int index = 2;
   for (var i = 0; i < num; i++) {
     int l = list.elementAt(index);
     ret[i] = utf8.decode(list.sublist(index + 1, index + 1 + l));
